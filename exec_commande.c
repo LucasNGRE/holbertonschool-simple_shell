3451efaab19a41f19079e786dcd9d2f6322f72dd
NamaKa298 <marion.saint-martin@protonmail.com>
2024-04-19 16:59:00 +0200

    modif exec_comande

--- exec_commande.c
+++ exec_commande.c
@@ -37,12 +37,12 @@ void execute(char **args)
  * le chemin complet de l'exécutable,
  *         ou NULL s'il n'est pas trouvé dans le PATH ou en cas d'erreur.
  */
-char **get_path(char **args)
+char get_path(char args)
 {
-	char *path = getenv("PATH");
-	char *copy = _strdup(path);
-	char *path_token;
-	char *alloc_memory_path;
+	char path = getenv("PATH");
+	charcopy = _strdup(path);
+	char path_token;
+	charalloc_memory_path;
 
 	if (copy == NULL)
 	{
@@ -56,23 +56,18 @@ char **get_path(char **args)
 		dinfo("path_token: %s\n", path_token);
 		alloc_memory_path = malloc(sizeof(char) *
 		(_strlen(path_token) + _strlen(args[0]) + 2));
-		if (alloc_memory_path == NULL)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
 		sprintf(alloc_memory_path, "%s/%s", path_token, args[0]);
 		if (access(alloc_memory_path, X_OK) == 0)
 		{
 			dsuccess("alloc_memory_path: %s\n", alloc_memory_path);
-			_strcpy(args[0], alloc_memory_path);
-			free (copy);
-			return (args);
+			_strcpy(copy, alloc_memory_path);
+			args[0] = alloc_memory_path;
+			break;
 		}
 		free(alloc_memory_path);
 		path_token = strtok(NULL, ":");
 	}
 	free(copy);
 
-	return (NULL);
+	return (args);
 }
